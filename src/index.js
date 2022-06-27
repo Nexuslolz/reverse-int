7d10fc366f16736201f3ca386d18b39f11cfc3b0
Nexuslolz <a.klyosov@inbox.ru>
2022-06-27 09:22:06 +0500

    feat: complete the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    let newArr = [];
+    let x;
+    if (n<0){
+         x = n* (-1);
+    }else{
+        x = n;
+    }
+    let str = String(x)
+    for(i=str.length - 1; i >= 0; i--){
+            newArr.push(str[i])
+        }
+        let result = newArr.join('')
+        return result;
 }
